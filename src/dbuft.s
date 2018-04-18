b20bf6201bdddaccfa59bec83b3057c35bc92d12
Michał Wilde <packerbat@gmail.com>
2018-04-18 11:14:29 +0200

    better animation in dbuft.s in text mode

--- src/dbuft.s
+++ src/dbuft.s
@@ -1,10 +1,17 @@
 ;---------------------------------------------------------- 
-; sekwencja uruchamiająca program bees
+; sekwencja uruchamiająca program dbuft
 
 .import INITT, SETDBT, SWAPSCRT, CLST, TXTPAGE
 
 .segment "ZEROPAGE":zeropage
 NPTR:  .res 2
+NRSLUP: .res 1
+
+.segment "DATA"
+SLUPKI:   .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+
+.segment "RODATA"
+KONCOWKI:  .byte 32,100,111,121,98,248,247,227
 
 .segment "CODE"
     .org $0801
@@ -16,33 +23,91 @@ NPTR:  .res 2
     .word 0           ;wskaźnik na następną linię, $0000 oznacza, że jest to ostania linia
 
     jsr INITT
-    ;lda #1
-    ;jsr SETDBT        ;turn off double buffer
+    lda #1
+    jsr SETDBT        ;turn off double buffer
 
 :   jsr DRAW_SCEEN
     jsr SWAPSCRT
     jmp :-
 
 .proc DRAW_SCEEN
-    lda #$FE
+    lda #$35
     jsr CLST
 
-    lda #0
+    lda #<(24*40)
     sta NPTR
     lda TXTPAGE
     clc
-    adc #3
+    adc #>(24*40)
     sta NPTR+1      ;będzie $6300 albo $6700
-    lda $A2         ;jiffy clock
-    and #$3F
-    ldx #4          ;4x256 = 1KB
-    ldy #$E8        ;rozmiar $03e8 = 1000
-:   dey
+
+    lda #0          ;numer kolumny
+    sta NRSLUP
+
+nastepny_slupek:
+    ldy NRSLUP
+    ldx SLUPKI,y
+    beq slupek_namalowany
+ 
+    lda NPTR+1
+    pha
+    lda NPTR
+    pha
+    ldy #0
+    cpx #8
+    bcc :++        ;czas na koncowke
+    
+:   lda #160        ;spacja w inwersji
     sta (NPTR),y
+    lda NPTR        ;bloczek wyżej
+    sec
+    sbc #40
+    sta NPTR
+    lda NPTR+1
+    sbc #0
+    sta NPTR+1
+    txa
+    sec
+    sbc #8
+    tax
+    cpx #8          ;wciąż mamy całe bloczki 
+    bcs :-
+
+:   cpx #0          ;malowanie_koncowki X=0..7
+    beq :+
+    lda KONCOWKI,x
+    sta (NPTR),y
+
+:   pla             ;powrót na dół ekranu
+    sta NPTR
+    pla
+    sta NPTR+1
+
+slupek_namalowany:
+    lda NPTR        ;nastepna kolumna
+    clc
+    adc #1
+    sta NPTR
+    lda NPTR+1
+    adc #0
+    sta NPTR+1
+
+    inc NRSLUP
+    lda NRSLUP
+    cmp #40
+    bcc nastepny_slupek
+    
+    ldx #0          ;przesunięcie słupków w lewo
+    ldy #39
+:   lda SLUPKI+1,x
+    sta SLUPKI,x
+    inx
+    dey
     bne :-
-    dec NPTR+1
-    dex
-    bne :-
+    lda $A2
+    lsr
+    lsr
+    sta SLUPKI,x
 
     ;jsr WAIT
     rts
